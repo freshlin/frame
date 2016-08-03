1e5ab72476c937adf5d546f92d09146538a6bc09
freshlin <1516264736@qq.com>
2016-08-03 22:43:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/freshlin/frame/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.freshlin.frame;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = this;
+
+        findViewById(R.id.itbtnTest).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context, TestActivity.class));
+            }
+        });
+    }
+}
