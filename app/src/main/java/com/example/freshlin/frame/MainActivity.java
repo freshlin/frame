5791d2f685a51f3432bc680bee30f7c09105440f
freshlin <1516264736@qq.com>
2016-08-03 22:50:49 +0800

    test

--- app/src/main/java/com/example/freshlin/frame/MainActivity.java
+++ app/src/main/java/com/example/freshlin/frame/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int xx = 1;
         setContentView(R.layout.activity_main);
         context = this;
 
